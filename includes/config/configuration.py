d7aafb5c0fdd92960322c51a827f023ec6623251
Ram-Pasupula <Ram-Pasupula>
2024-11-28 13:53:07 +0000

    conf
    c

---
+++ includes/config/configuration.py
@@ -0,0 +1,6 @@
+# Databricks notebook source
+raw_folder_path = "/mnt/azuredbstorageac/raw"
+demo_folder_path = "/mnt/azuredbstorageac/demo"
+processed_folder_path = "/mnt/azuredbstorageac/processed"
+
+
